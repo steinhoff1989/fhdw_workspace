6c5737cebec59c4726f79901a26d3d225ee0e7b8
Sebastian Steinhoff <steinhoff1989@gmail.com>
2016-05-06 17:29:04 +0200

    todo: states with cached value

---
+++ calculator/src/model/AObservee.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public abstract class AObservee implements Observee{
+
+	private List<Observer> observers;
+	
+	public AObservee(){
+		this.observers = new ArrayList<Observer>();
+	}
+	
+	public void register(Observer o){
+		observers.add(o);
+	}
+	
+	public void deregister(Observer o){
+		observers.remove(o);
+	}
+	
+	public void notifyObservers(){
+		Iterator<Observer> iterator = observers.iterator();
+		while(iterator.hasNext()){
+			Observer current = iterator.next();
+			current.update();
+		}
+	}
+}
---
+++ calculator/src/model/Observee.java
@@ -0,0 +1,10 @@
+package model;
+
+public interface Observee {
+
+	public void register(Observer o);
+	
+	public void deregister(Observer o);
+	
+	public void notifyObservers();
+}
