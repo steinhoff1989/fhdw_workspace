17e585df668f5cfbd4fd673ade6a30879ac7c4fd
Felix Wollenweber <felix.wollenweber@gmail.com>
2016-10-21 17:12:51 +0200

    v0.1

--- Sort/src/merge/MergeSort.java
+++ Sort/src/merge/MergeSort.java
@@ -1,41 +1,63 @@
 package merge;
 
 import bubble.Buffer;
+import bubble.Buffer.StoppException;
 
 public class MergeSort {
 	
 	private Buffer<Comparable> inputBuffer;
-	private Buffer<Comparable> divideBuffer1;
-	private Buffer<Comparable> divideBuffer2;
+	private Buffer<Comparable> resultBuffer;
 
 
+	@SuppressWarnings("rawtypes")
 	public MergeSort(Buffer<Comparable> inputBuffer) {
 		this.inputBuffer = inputBuffer;
-		this.divideBuffer1 = new Buffer<Comparable>();
-		this.divideBuffer2 = new Buffer<Comparable>();
+		this.resultBuffer = new Buffer<Comparable>();
 
 	}
 	
+	public void sort() {
+		if(this.inputBuffer.size() >2){
+			this.divide();
+		}
+		else{
+			this.merge();
+		}
+	}
+
+	private void merge() {
+		
+	};
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public void divide(){
 		int halfSize = this.inputBuffer.size() / 2;
 		int otherSize = this.inputBuffer.size() - halfSize;
 		
 		for(int i=0; i<=halfSize; i++){
-			Comparable element = inputBuffer.get();
-			this.divideBuffer1.put(element);
+			Comparable element;
+			try {
+				element = this.inputBuffer.get();
+				Buffer divideBuffer1 = new Buffer<Comparable>();
+				divideBuffer1.put(element);
+				MergeSort tempSort1 = new MergeSort(divideBuffer1);
+				tempSort1.sort();
+			} catch (StoppException e) {
+				e.printStackTrace();
+			}
 		}
 		for (int i=0; i<=otherSize; i++){
-			Comparable element = inputBuffer.get();
-			this.divideBuffer2.put(element);
+			Comparable element;
+			try {
+				element = this.inputBuffer.get();
+				Buffer divideBuffer2 = new Buffer<Comparable>();
+				divideBuffer2.put(element);
+				MergeSort tempSort2 = new MergeSort(divideBuffer2);
+				tempSort2.sort();
+			} catch (StoppException e) {
+				e.printStackTrace();
+			}
 		}
-		this.sort();
-
 }
-
-	public void sort() {
-		if(this.divideBuffer1.size()>1){
-			
-		}
-	};
 
 }
