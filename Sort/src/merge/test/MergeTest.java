bafe8e894d6803bf7223759d363d2681642e1b4e
Felix Wollenweber <felix.wollenweber@gmail.com>
2016-10-21 17:23:24 +0200

    Testlasche hinzugefügt

---
+++ Sort/src/merge/test/MergeTest.java
@@ -0,0 +1,14 @@
+package merge.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MergeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
