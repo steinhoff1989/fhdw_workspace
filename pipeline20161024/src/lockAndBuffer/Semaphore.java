0844ce1c9eda0bd210fdbaca0012a4d37ebbdb98
Sebastian Steinhoff <steinhoff1989@gmail.com>
2016-10-24 08:55:59 +0200

    mitschrift 20161024 1. hälfte

--- pipeline20161024/src/lockAndBuffer/Semaphore.java
+++ pipeline20161024/src/lockAndBuffer/Semaphore.java
@@ -2,22 +2,38 @@ package lockAndBuffer;
 
 public class Semaphore implements AbstractSemaphore{
 
+	private int counter;
+	private Lock waiting;
+	private Lock ka;
+	
+
 	/**Creates a semaphore with the internal counter set to the value
 	 * of the parameter.
 	 */
 	public Semaphore(int count) {
-		
+		if(count<0) throw new Error();
+		this.counter = count;
+		this.waiting = new Lock(true);
+		this.ka = new Lock(false);
 	}
 	@Override
 	public void down() {
-		// TODO Auto-generated method stub
-		
+		this.ka.lock();
+		while(this.counter == 0){
+			this.ka.unlock();
+			this.waiting.lock();
+			this.ka.lock();
+		}
+		this.counter--;
+		this.ka.unlock();
 	}
 
 	@Override
 	public void up() {
-		// TODO Auto-generated method stub
-		
+		this.ka.lock();
+		this.counter++;
+		this.waiting.unlock();
+		this.ka.unlock();
 	}
 
 }
