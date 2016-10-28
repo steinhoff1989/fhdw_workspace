0cf3dd3f0e7126e6e590da926699c67e7ecc9c60
Felix Wollenweber <felix.wollenweber@gmail.com>
2016-10-28 14:28:35 +0200

    merge advanced

--- Sort/src/merge/MergeSort.java
+++ Sort/src/merge/MergeSort.java
@@ -1,63 +1,130 @@
 package merge;
 
-import bubble.Buffer;
-import bubble.Buffer.StoppException;
+import merge.Buffer.StoppException;
 
-public class MergeSort {
+public class MergeSort<T extends Comparable<T>> {
 	
-	private Buffer<Comparable> inputBuffer;
-	private Buffer<Comparable> resultBuffer;
+	private Buffer<T> inputBuffer;
+	private Buffer<T> resultBuffer;
 
 
-	@SuppressWarnings("rawtypes")
-	public MergeSort(Buffer<Comparable> inputBuffer) {
+	public MergeSort(Buffer<T> inputBuffer) {
 		this.inputBuffer = inputBuffer;
-		this.resultBuffer = new Buffer<Comparable>();
-
+		this.resultBuffer = new Buffer<T>();
 	}
 	
 	public void sort() {
 		if(this.inputBuffer.size() >2){
 			this.divide();
 		}
-		else{
-			this.merge();
+		else{ 
+			try {
+			this.resultBuffer.put(this.inputBuffer.get());
+			this.resultBuffer.put(this.inputBuffer.get());
+		} catch (StoppException e) {
+			this.resultBuffer.stopp();
+			}
 		}
 	}
 
-	private void merge() {
-		
-	};
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public void divide(){
 		int halfSize = this.inputBuffer.size() / 2;
 		int otherSize = this.inputBuffer.size() - halfSize;
 		
-		for(int i=0; i<=halfSize; i++){
-			Comparable element;
+		Buffer<T> divideBuffer1 = new Buffer<T>();
+		Buffer<T> divideBuffer2 = new Buffer<T>();
+		
+		for(int i=0; i<halfSize; i++){
 			try {
-				element = this.inputBuffer.get();
-				Buffer divideBuffer1 = new Buffer<Comparable>();
+				T element = this.inputBuffer.get();
 				divideBuffer1.put(element);
-				MergeSort tempSort1 = new MergeSort(divideBuffer1);
-				tempSort1.sort();
 			} catch (StoppException e) {
-				e.printStackTrace();
+				divideBuffer1.stopp();
 			}
 		}
-		for (int i=0; i<=otherSize; i++){
-			Comparable element;
+		divideBuffer1.stopp();
+		
+		for (int i=0; i<otherSize; i++){
 			try {
-				element = this.inputBuffer.get();
-				Buffer divideBuffer2 = new Buffer<Comparable>();
+				T element = this.inputBuffer.get();
 				divideBuffer2.put(element);
-				MergeSort tempSort2 = new MergeSort(divideBuffer2);
-				tempSort2.sort();
 			} catch (StoppException e) {
-				e.printStackTrace();
+				divideBuffer2.stopp();
 			}
 		}
+
+		MergeSort<T> tempSort1 = new MergeSort<T>(divideBuffer1);
+		MergeSort<T> tempSort2 = new MergeSort<T>(divideBuffer2);
+
+		tempSort1.sort();
+		tempSort2.sort();
+		
+		this.merge(tempSort1,tempSort2);
 }
+
+	private void merge(MergeSort<T> tempSort1, MergeSort<T> tempSort2) {
+		
+		try {
+			T first = tempSort1.resultBuffer.get();
+			try {
+				T second = tempSort2.resultBuffer.get();
+				if(first.compareTo(second)<0){
+					this.resultBuffer.put(first);
+//					this.resultBuffer.put(second);
+					this.mergeHelper(tempSort1, second);
+				}
+				else{
+					this.resultBuffer.put(second);
+					this.mergeHelper(tempSort2, first);
+//					this.resultBuffer.put(first);
+				}
+//				this.merge(tempSort1, tempSort2);
+				
+			} catch (StoppException e) {
+				this.resultBuffer.put(first);
+				this.resultBuffer.stopp();
+			}
+		} catch (StoppException e) {
+			try {
+				T second = tempSort2.resultBuffer.get();
+				this.resultBuffer.put(second);
+				this.resultBuffer.stopp();
+			} catch (StoppException e1) {
+				this.resultBuffer.stopp();
+			}
+			
+			}
+	}
+	private void mergeHelper(MergeSort<T> tempSort, T element){
+			try {
+				T first = tempSort.resultBuffer.get();
+				if(first.compareTo(element) < 0){
+					this.resultBuffer.put(first);
+					mergeHelper(tempSort, element);
+				}else{
+					this.resultBuffer.put(element);
+					mergeHelper(tempSort1, first);
+				}
+			} catch (StoppException e) {
+				this.resultBuffer.put(element);
+				this.resultBuffer.stopp();
+			}
+	}
+	
+	public Buffer<T> getInputBuffer() {
+		return inputBuffer;
+	}
+
+	public void setInputBuffer(Buffer<T> inputBuffer) {
+		this.inputBuffer = inputBuffer;
+	}
+
+	public Buffer<T> getResultBuffer() {
+		return resultBuffer;
+	}
+
+	public void setResultBuffer(Buffer<T> resultBuffer) {
+		this.resultBuffer = resultBuffer;
+	}
 
 }
--- Sort/src/merge/test/MergeTest.java
+++ Sort/src/merge/test/MergeTest.java
@@ -4,11 +4,142 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import merge.Buffer;
+import merge.Buffer.StoppException;
+import merge.MergeSort;
+
 public class MergeTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test8Var() throws StoppException {
+		Buffer<Integer> inputBuffer = new Buffer<Integer>();
+		
+		inputBuffer.put(2);
+		inputBuffer.put(3);
+		inputBuffer.put(5);
+		inputBuffer.put(1);
+		inputBuffer.put(0);
+		inputBuffer.put(7);
+		inputBuffer.put(9);
+		inputBuffer.put(4);
+		inputBuffer.stopp();
+		
+		MergeSort<Integer> ms = new MergeSort<>(inputBuffer);
+		
+		ms.sort();
+		
+		assertEquals(0, ms.getResultBuffer().get().intValue());
+		assertEquals(1, ms.getResultBuffer().get().intValue());
+		assertEquals(2, ms.getResultBuffer().get().intValue());
+		assertEquals(3, ms.getResultBuffer().get().intValue());
+		assertEquals(4, ms.getResultBuffer().get().intValue());
+		assertEquals(5, ms.getResultBuffer().get().intValue());
+		assertEquals(7, ms.getResultBuffer().get().intValue());
+		assertEquals(9, ms.getResultBuffer().get().intValue());
 	}
 
+	@Test
+	public void test1Var() throws StoppException {
+		Buffer<Integer> inputBuffer = new Buffer<Integer>();
+		
+		inputBuffer.put(2);
+		inputBuffer.stopp();
+		
+		MergeSort<Integer> ms = new MergeSort<>(inputBuffer);
+		
+		ms.sort();
+		
+		assertEquals(2, ms.getResultBuffer().get().intValue());
+	}
+	
+	@Test
+	public void test2VarSorted() throws StoppException {
+		Buffer<Integer> inputBuffer = new Buffer<Integer>();
+		
+		inputBuffer.put(2);
+		inputBuffer.put(3);
+		inputBuffer.stopp();
+		
+		MergeSort<Integer> ms = new MergeSort<>(inputBuffer);
+		
+		ms.sort();
+		
+		assertEquals(2, ms.getResultBuffer().get().intValue());
+		assertEquals(3, ms.getResultBuffer().get().intValue());
+	}
+	
+	@Test
+	public void test2VarUnsorted() throws StoppException {
+		Buffer<Integer> inputBuffer = new Buffer<Integer>();
+		
+		inputBuffer.put(3);
+		inputBuffer.put(2);
+		inputBuffer.stopp();
+		
+		MergeSort<Integer> ms = new MergeSort<>(inputBuffer);
+		
+		ms.sort();
+		
+		assertEquals(2, ms.getResultBuffer().get().intValue());
+		assertEquals(3, ms.getResultBuffer().get().intValue());
+	}
+	@Test
+	public void test3Var() throws StoppException {
+		Buffer<Integer> inputBuffer = new Buffer<Integer>();
+		
+		inputBuffer.put(3);
+		inputBuffer.put(2);
+		inputBuffer.put(0);
+		inputBuffer.stopp();
+		
+		MergeSort<Integer> ms = new MergeSort<>(inputBuffer);
+		
+		ms.sort();
+		
+		assertEquals(0, ms.getResultBuffer().get().intValue());
+		assertEquals(2, ms.getResultBuffer().get().intValue());
+		assertEquals(3, ms.getResultBuffer().get().intValue());
+	}
+	@Test
+	public void test4Var() throws StoppException {
+		Buffer<Integer> inputBuffer = new Buffer<Integer>();
+		
+		inputBuffer.put(3);
+		inputBuffer.put(2);
+		inputBuffer.put(9);
+		inputBuffer.put(0);
+		inputBuffer.stopp();
+		
+		MergeSort<Integer> ms = new MergeSort<>(inputBuffer);
+		
+		ms.sort();
+		
+		assertEquals(0, ms.getResultBuffer().get().intValue());
+		assertEquals(2, ms.getResultBuffer().get().intValue());
+		assertEquals(3, ms.getResultBuffer().get().intValue());
+		assertEquals(9, ms.getResultBuffer().get().intValue());
+
+	}
+	@Test
+	public void test5Var() throws StoppException {
+		Buffer<Integer> inputBuffer = new Buffer<Integer>();
+		
+		inputBuffer.put(3);
+		inputBuffer.put(2);
+		inputBuffer.put(9);
+		inputBuffer.put(0);
+		inputBuffer.put(5);
+		inputBuffer.stopp();
+		
+		MergeSort<Integer> ms = new MergeSort<>(inputBuffer);
+		
+		ms.sort();
+		
+		assertEquals(0, ms.getResultBuffer().get().intValue());
+		assertEquals(2, ms.getResultBuffer().get().intValue());
+		assertEquals(3, ms.getResultBuffer().get().intValue());
+		assertEquals(5, ms.getResultBuffer().get().intValue());
+		assertEquals(9, ms.getResultBuffer().get().intValue());
+
+	}
 }
