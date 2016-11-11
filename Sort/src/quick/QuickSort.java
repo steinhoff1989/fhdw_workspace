56d560623b26a7a92ed2591dc5ee9d3ab36175d2
Sebastian Steinhoff <steinhoff1989@gmail.com>
2016-11-11 19:10:50 +0100

    quicksort: passed all testcases

--- Sort/src/quick/QuickSort.java
+++ Sort/src/quick/QuickSort.java
@@ -7,75 +7,75 @@ public class QuickSort<T extends Comparable<T>> extends Process<T> {
 
 	private Buffer<T> insertBuffer;
 	private Buffer<T> resultBuffer;
-	private T referenceElement;
-	
+
 	private Buffer<T> leftList;
 	private Buffer<T> rightList;
-	
+
+	private T referenceElement;
+
 	private QuickSort<T> sortLeftList;
 	private QuickSort<T> sortRightList;
-	private Buffer<T> firstElement;
-	
+
 	public QuickSort(Buffer<T> insertBuffer) {
 		this.insertBuffer = insertBuffer;
 		this.resultBuffer = new Buffer<T>();
-		this.firstElement = new Buffer<T>();
-
 		this.leftList = new Buffer<T>();
 		this.rightList = new Buffer<T>();
-		try {
-			this.referenceElement = insertBuffer.get();
-			try{
-				this.firstElement.put(insertBuffer.get());
-				this.firstElement.stopp();
-			}catch (StoppException e) {
-				this.resultBuffer.put(referenceElement);
-				this.resultBuffer.stopp();
-			}
-		} catch (StoppException e) {
-			this.resultBuffer.stopp();
-		}
 	}
 
 	@Override
 	public void calculate() {
-		T nextElement;
-		try {
-			try{
-				nextElement = this.firstElement.get();	
-			}catch(StoppException ex){
-				nextElement = this.insertBuffer.get();
+		if(referenceElement == null){
+			try {
+				this.referenceElement = insertBuffer.get();
+				sortLeftList = new QuickSort<T>(this.leftList);
+				sortLeftList.startThread();
+				sortRightList = new QuickSort<T>(this.rightList);
+				sortRightList.startThread();
+			} catch (StoppException e) {
+				this.resultBuffer.stopp();
+				this.stopThread();
+				return;
 			}
-			if(nextElement.compareTo(referenceElement) <= 0){
+		}
+		
+		try {
+			T nextElement = this.insertBuffer.get();
+			if (nextElement.compareTo(referenceElement) <= 0) {
 				this.leftList.put(nextElement);
-			}else{
+
+			} else {
 				this.rightList.put(nextElement);
 			}
 		} catch (StoppException e) {
-			this.leftList.put(referenceElement);
 			this.leftList.stopp();
 			this.rightList.stopp();
-			
-			try {
-				this.sortLeftList = new QuickSort<>(leftList);
-				T left = this.sortLeftList.getResultBuffer().get();
-				this.sortLeftList.startThread();
-				this.resultBuffer.put(left);
-			} catch (StoppException e1) {
-				try {
-					this.sortRightList = new QuickSort<>(rightList);
-					T right = this.sortRightList.getResultBuffer().get();
-					this.sortRightList.startThread();
-					this.resultBuffer.put(right);
-				} catch (StoppException e2) {
-					this.resultBuffer.stopp();
-					this.stopThread();
-				}
-			}
+			this.stopThread();
+			this.copyFromLeftSortedBufferToResultBuffer();
+			this.resultBuffer.put(referenceElement);
+			this.copyFromRightSortedBufferToResultBuffer();
+			this.resultBuffer.stopp();
+		}
+	}
+
+	private void copyFromRightSortedBufferToResultBuffer() {
+		try {
+			this.resultBuffer.put(this.sortRightList.getResultBuffer().get());
+			copyFromRightSortedBufferToResultBuffer();
+		} catch (StoppException e) {
 			
 		}
 	}
-	
+
+	private void copyFromLeftSortedBufferToResultBuffer() {
+		try {
+			this.resultBuffer.put(this.sortLeftList.getResultBuffer().get());
+			copyFromLeftSortedBufferToResultBuffer();
+		} catch (StoppException e) {
+			
+		}
+	}
+
 	public Buffer<T> getResultBuffer() {
 		return resultBuffer;
 	}
